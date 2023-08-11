543ad53e8a4fc6765e07996e41b168c016bd194e
sedrickGerards <99662353+sedrickGerards@users.noreply.github.com>
2023-08-11 15:47:29 +0100

    arrow function with parameters.js

---
+++ JavaScript Algorithms and Data Structures/ES6/arrow function with parameters.js
@@ -0,0 +1,15 @@
+// var myConcat = function(arr1, arr2) {
+//   return arr1.concat(arr2);
+// };
+
+
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2)
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Just like a regular function, you can pass arguments into an arrow function.
+
+// If an arrow function has a single parameter, the parentheses enclosing the parameter may be omitted.
+
+// It is possible to pass more than one argument into an arrow function
